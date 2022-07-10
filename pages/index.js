c7bab9bbf34b70fb35efd88346021070a8c97853
David <david.shariev.2002@gmail.com>
2022-07-10 22:18:42 +0300

    images

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import Layout from "../components/layout";
+
+import image_1 from "../image/image_1.jpg";
 
 function HomePage() {
   return (
     <Layout>
       <h1>Hello, NextJS!</h1>
+      <Image src={image_1} alt="some img"></Image>
     </Layout>
   );
 }
